529eb4a4cb27567ba508a016a25e3b7bab13eb86
RonaldoMessias <ronaldo.messias@kabum.com.br>
2022-09-06 17:27:52 -0300

    feat: added new test to cover all line to SimulateFreight

--- test/integration/SimulateFreight.test.ts
+++ test/integration/SimulateFreight.test.ts
@@ -14,3 +14,16 @@ test("Deve simular o frete de um pedido", function () {
     const output = simulateFreight.execute(input);
     expect(output.total).toBe(260);
 })
+
+test("Deve rotornar uma exceção ao tentar simular o frete de um pedido com um item inexistente", function () {
+    const itemRepository = new ItemRepositoryMemory();
+    const simulateFreight = new SimulateFreight(itemRepository);
+    const input = {
+        orderItems: [
+            {idItem: 1,  quantity: 1},
+            {idItem: 2,  quantity: 1},
+            {idItem: 4,  quantity: 3}
+        ]
+    };
+    expect(() => simulateFreight.execute(input)).toThrow(new Error("Item not found"));
+})

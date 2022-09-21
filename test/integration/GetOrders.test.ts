91a3e34b84492ab712af42ed028097aa73f04231
ericbuenov <eric.bueno@kabum.com.br>
2022-09-21 15:30:26 -0300

    test: testando listagem de pedidos

---
+++ test/integration/GetOrders.test.ts
@@ -0,0 +1,41 @@
+import GetOrders from "../../src/application/usecase/get-orders/GetOrders";
+import PlaceOrder from "../../src/application/usecase/place-order/PlaceOrder";
+import RepositoryFactory from "../../src/domain/factory/RepositoryFactory";
+import Connection from "../../src/infra/database/Connection";
+import PostgreSQLConnectionAdapter from "../../src/infra/database/PostgreSQLConnectionAdapter";
+import DatabaseRepositoryFactory from "../../src/infra/factory/DatabaseRepositoryFactory";
+
+let connection: Connection;
+let repositoryFactory: RepositoryFactory;
+
+beforeEach(async function () {
+	connection = new PostgreSQLConnectionAdapter();
+	repositoryFactory = new DatabaseRepositoryFactory(connection);
+	const orderRepository = repositoryFactory.createOrderRepository();
+	await orderRepository.clean();
+});
+
+test("Deve obter a lista de pedidos", async function () {
+	const placeOrder = new PlaceOrder(repositoryFactory);
+	const input = {
+		cpf: "935.411.347-80",
+		orderItems: [
+			{ idItem: 1, quantity: 1},
+			{ idItem: 2, quantity: 1},
+			{ idItem: 3, quantity: 3}
+		],
+		coupon: "VALE20",
+		issueDate: new Date("2021-03-01T10:00:00")
+	};
+	await placeOrder.execute(input);
+	await placeOrder.execute(input);
+	await placeOrder.execute(input);
+	const getOrders = new GetOrders(repositoryFactory);
+	const output = await getOrders.execute();
+	console.log(output);
+	expect(output).toHaveLength(3);
+});
+
+afterEach(async function () {
+	await connection.close();
+});
--- test/integration/OrderRepositoryDatabase.test.ts
+++ test/integration/OrderRepositoryDatabase.test.ts
@@ -5,33 +5,51 @@ import Order from "../../src/domain/entity/Order";
 import Connection from "../../src/infra/database/Connection";
 import PostgreSQLConnectionAdapter from "../../src/infra/database/PostgreSQLConnectionAdapter";
 import OrderRepositoryDatabase from "../../src/infra/repository/database/OrderRepositoryDatabase";
-import OrderRepositoryMemory from "../../src/infra/repository/memory/OrderRepositoryMemory";
 
 let connection: Connection;
 
 beforeEach(function () {
-    connection = new PostgreSQLConnectionAdapter();
-})
+	connection = new PostgreSQLConnectionAdapter();
+});
 
-test("Deve salvar um pedido", async function() {
-    const orderRepository = new OrderRepositoryDatabase(connection);
-    // const orderRepository = new OrderRepositoryMemory();
-    await orderRepository.clean();
-    const order = new Order("219.858.810-20", new Date("2021-03-01T10:00:00"), 1);
-    order.addItem(new Item(1, "Instrumentos Musicais", "Guitarra", 1000, new Dimension(100, 30, 10), 3), 1);
-    order.addItem(new Item(2, "Instrumentos Musicais", "Amplificador", 5000, new Dimension(100, 50, 50), 20), 1);
-    order.addItem(new Item(3, "Instrumentos Musicais", "Cabo", 30, new Dimension(10, 10, 10), 1), 3);
-    const coupon = new Coupon("VALE20", 20);
-    order.addCoupon(coupon);
-    await orderRepository.save(order);
-    const count = await orderRepository.count();
-    expect(count).toBe(1);
-    const savedOrder = await orderRepository.getByCode("202100000001");
-    expect(savedOrder.code.value).toBe("202100000001");
-    expect(savedOrder.getTotal()).toBe(5132);
-    expect(savedOrder.freight.getTotal()).toBe(260);
-})
+test("Deve salvar um pedido", async function () {
+	const orderRepository = new OrderRepositoryDatabase(connection);
+	await orderRepository.clean();
+	const order = new Order("935.411.347-80", new Date("2021-03-01T10:00:00"), 1);
+	order.addItem(new Item(1, "Instrumentos Musicais", "Guitarra", 1000, new Dimension(100, 30, 10), 3), 1);
+	order.addItem(new Item(2, "Instrumentos Musicais", "Amplificador", 5000, new Dimension(100, 50, 50), 20), 1);
+	order.addItem(new Item(3, "Instrumentos Musicais", "Cabo", 30, new Dimension(10, 10, 10), 1), 3);
+	const coupon = new Coupon("VALE20", 20);
+	order.addCoupon(coupon);
+	await orderRepository.save(order);
+	const count = await orderRepository.count();
+	expect(count).toBe(1);
+	const savedOrder = await orderRepository.getByCode("202100000001");
+	expect(savedOrder.code.value).toBe("202100000001");
+	expect(savedOrder.getTotal()).toBe(5132);
+	expect(savedOrder.freight.getTotal()).toBe(260);
+});
+
+test("Deve listar os pedidos", async function () {
+	const orderRepository = new OrderRepositoryDatabase(connection);
+	await orderRepository.clean();
+	const order = new Order("935.411.347-80", new Date("2021-03-01T10:00:00"), 1);
+	order.addItem(new Item(1, "Instrumentos Musicais", "Guitarra", 1000, new Dimension(100, 30, 10), 3), 1);
+	order.addItem(new Item(2, "Instrumentos Musicais", "Amplificador", 5000, new Dimension(100, 50, 50), 20), 1);
+	order.addItem(new Item(3, "Instrumentos Musicais", "Cabo", 30, new Dimension(10, 10, 10), 1), 3);
+	const coupon = new Coupon("VALE20", 20);
+	order.addCoupon(coupon);
+	await orderRepository.save(order);
+	await orderRepository.save(order);
+	await orderRepository.save(order);
+	const count = await orderRepository.count();
+	expect(count).toBe(3);
+	const savedOrders = await orderRepository.getAll();
+	expect(savedOrders).toHaveLength(3);
+	const [savedOrder1, savedOrder2, savedOrder3] = savedOrders;
+	expect(savedOrder3.getTotal()).toBe(5132);
+});
 
 afterEach(async function () {
-    await connection.close();
-})
+	await connection.close();
+});
